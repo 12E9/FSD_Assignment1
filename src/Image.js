038c0093b6ba60f7f41cbcd4a4f874f05f25aedd
Your Name <you@example.com>
2022-02-11 03:32:33 -0600

    assignment1

---
+++ src/Image.js
@@ -0,0 +1,7 @@
+import lizard from './images/Lizard.jpg';
+function Image(){
+    return(
+        <img src = {lizard} />
+    )
+}
+export default Image;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './styles.css';
+
+import Image from './Image.js';
+
+
+ReactDOM.render(
+    <center>
+    <div class="card">
+        <Image />
+        <h4><b>LIZARD</b></h4>
+        <p>Lizards are a widespread group of squamate reptiles,with over 6,000 species,ranging across all continents except Antartica</p>
+        <h5>Share &nbsp;&nbsp;&nbsp;&nbsp; Learn more</h5>
+    </div>
+    </center>, 
+    document.getElementById("root")
+
+);
